cac9bc61513bcc7286c8ef7a75be12fbf2fff56e
Nono <chris5215@gmail.com>
2020-07-28 16:33:08 +0800

    make it master

--- app.py
+++ app.py
@@ -4,10 +4,25 @@
 # In[ ]:
 
 
+# import gspread
+# from oauth2client.service_account import ServiceAccountCredentials
+# scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/drive']
+# creds = ServiceAccountCredentials.from_json_keyfile_name("./creds.json", scope)
+# client = gspread.authorize(creds)
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name("./creds.json", scope)
+
+# based on https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html
+# read that file for how to generate the creds and how to use gspread to read and write to the spreadsheet
+
+# use creds to create a client to interact with the Google Drive API
+scopes = ['https://spreadsheets.google.com/feeds']
+json_creds = os.getenv("GOOGLE_SHEETS_CREDS_JSON")
+
+creds_dict = json.loads(json_creds)
+creds_dict["private_key"] = creds_dict["private_key"].replace("\\\\n", "\n")
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scopes)
 client = gspread.authorize(creds)
 
 
