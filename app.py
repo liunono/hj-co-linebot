e8e0ded8bebc57df6b133d746c479fd21cbb2d76
Nono <chris5215@gmail.com>
2020-07-28 14:26:21 +0800

    make it master

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name("./creds.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(creds)
 
 
